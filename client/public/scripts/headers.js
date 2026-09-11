5034e8d5713bb4c5ea1f9a8300082c469e5d2f3e
benduse <110694311+benduse@users.noreply.github.com>
2026-09-10 23:08:43 -0700

    added scripts to create headers and logos added through javascript. on step number 6 but the code is not working

---
+++ client/public/scripts/headers.js
@@ -0,0 +1,36 @@
+const header = document.querySelector('header')
+
+const headerContainer = document.createElement('div')
+headerContainer.className = 'header-container'
+
+const headerLeft =document.createElement('div')
+headerLeft.className = 'header-left'
+
+const img = document.createElement('img')
+img.src = '/logo.png'
+img.alt = 'Logo'
+
+
+const headerTitle = document.createElement('h1')
+headerTitle.textContent = 'UnEarthed'
+
+
+headerLeft.appendChild(headerLogo)
+headerLeft.appendChild(headerTitle)
+
+const headerRight = document.createElement('div')
+headerRight.className = 'header-right'
+
+const headerButton = document.createElement('button')
+headerButton.textContent = 'Home'
+
+headerButton.addEventListener('click', function handleClick(event){
+    window.location='/'
+})
+
+headerRight.appendChild(headerButton)
+
+headerContainer.appendChild(headerLeft)
+headerContainer.appendChild(headerRight)
+
+header.appendChild(headerContainer)
